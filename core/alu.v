037d21c38afa2576957993e2bb03eacdd9f3d498
Yusron Izza <yusronizzafaradisa@gmail.com>
2024-12-31 13:02:20 +0700

    Fix carry and borrow bit in ALU SUB operation. Add more instruction test.

--- core/alu.v
+++ core/alu.v
@@ -26,6 +26,9 @@ wire flag_V;
 
 // Wire for operation result
 wire [32:0] a_sum_b;
+wire carry_out;
+wire [32:0] a_sub_b;
+wire borrow_in;
 wire [31:0] a_and_b;
 wire [31:0] a_or_b;
 wire [31:0] a_slt_b;
@@ -34,14 +37,13 @@ wire [31:0] a_sltu_b;
 wire [31:0] a_xor_b;
 wire [31:0] a_srl_b;
 wire [31:0] a_sra_b;
-wire [32:0] mux_out;
-wire [31:0] carry_in = 32'h00000001;
-wire carry_out;
+wire [32:0] mux_out;;
 
 // Perform operation
-assign mux_out      = (ALUControl == SUB) ? (~srcB + carry_in): srcB;
-assign a_sum_b      = srcA + mux_out;
+assign a_sum_b      = srcA + srcB;
 assign carry_out    = a_sum_b[32];
+assign a_sub_b      = {1'b1, srcA} + {1'b0, ~srcB} + 32'h00000001;
+assign borrow_in    = a_sub_b[32];
 assign a_and_b      = srcA & srcB;
 assign a_or_b       = srcA | srcB;
 assign a_slt_b      = {30'b000000000000000000000000000000, (a_sum_b[31] ^ flag_V)};
@@ -51,20 +53,11 @@ assign a_xor_b      = srcA ^ srcB;
 assign a_srl_b      = srcA >> srcB[4:0];
 assign a_sra_b      = srcA >>> srcB[4:0];
 
-// Operation for assigning the flags
-assign flag_N       = ALUResult[31];
-assign flag_Z       = &(~ALUResult);
-assign flag_C       = carry_out & (~ALUControl[1]);
-assign flag_V       = (~ALUControl[1]) & (srcA[31] ^ a_sum_b[31]) & (~(ALUControl[0] ^ srcA[31] ^ srcB[31]));
-
-// Concat the final flags and MUX for the final ALU result
-assign flags        = {flag_N, flag_Z, flag_C, flag_V};
-
 /* MUX to select correct result */
 always @* begin
     case (ALUControl)
         ADD     : ALUResult = a_sum_b[31:0];
-        SUB     : ALUResult = a_sum_b[31:0];
+        SUB     : ALUResult = a_sub_b[31:0];
         AND     : ALUResult = a_and_b;
         OR      : ALUResult = a_or_b;
         SLT     : ALUResult = a_slt_b;
@@ -76,5 +69,18 @@ always @* begin
         default : ALUResult = 32'h00000000;
     endcase
 end
+
+// Operation for assigning the flags
+assign flag_N       = (ALUControl == ADD) ? ALUResult[31] :
+                      (ALUControl == SUB) ? ALUResult[31] :
+                      (ALUControl == SLT) ? ALUResult[31] :
+                      (ALUControl == SRA) ? ALUResult[31] : 1'b0;
+assign flag_Z       = &(~ALUResult);
+assign flag_C       = (ALUControl == ADD) ? carry_out :
+                      (ALUControl == SUB) ? ~borrow_in : 1'b0;
+assign flag_V       = (~ALUControl[1]) & (srcA[31] ^ a_sum_b[31]) & (~(ALUControl[0] ^ srcA[31] ^ srcB[31]));
+
+// Concat the final flags and MUX for the final ALU result
+assign flags        = {flag_N, flag_Z, flag_C, flag_V};
 
 endmodule
